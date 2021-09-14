5194bbf876347f993eb127d1cf6abe15c0e329b0
kei2357 <morimorio777@gmail.com>
2021-09-14 03:41:08 +0000

    9.14 完了　他ユーザーのアクセス制限

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,6 +3,8 @@ class Attendance < ApplicationRecord
 
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
+  # validates :finished_at, presence: true
+  # validates :started_at, presence: true
 
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at

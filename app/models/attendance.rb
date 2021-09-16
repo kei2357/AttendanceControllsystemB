ad5e0d059642f54a1b85690a4fa599d05b9f81a6
kei2357 <morimorio777@gmail.com>
2021-09-16 05:15:55 +0000

    9.16 完了　他ユーザーのアクセス制限

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,13 +3,15 @@ class Attendance < ApplicationRecord
 
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
-  # validates :finished_at, presence: true
-  # validates :started_at, presence: true
+
 
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
+  # 出勤時間と退勤時間が両方存在する場合に更新可能
+  validate :update_invalid_without_started_at_and_finished_at, on: :update
+
 
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
@@ -20,4 +22,12 @@ class Attendance < ApplicationRecord
       errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
     end
   end
+  
+  def update_invalid_without_started_at_and_finished_at
+    if started_at.present? && finished_at.present?
+    else
+      errors
+    end
+  end
 end
+
